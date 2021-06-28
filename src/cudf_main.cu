eeac10c24e6f6786fd2b7d9b12ad111096ef66e8
Alexander <aocsa.cs@gmail.com>
2021-06-28 17:57:17 -0500

    dyanmic dispatcher

--- src/cudf_main.cu
+++ src/cudf_main.cu
@@ -9,12 +9,19 @@ DEFINE_DISPATCH(select_stub);
 
 void test_arrow_select(){
   auto backend = execution_backend(backend_id::ARROW);
-//  backend_dispatcher(id, select_functor(), table_view, sortColIndices);
   select_stub(backend, 0, 5, 8);
 }
 
- 
+
+void test_cudf_select(){
+  auto backend = execution_backend(backend_id::CUDF);
+  select_stub(backend, 0, 5, 8);
+}
+
+
 int main() {
   test_arrow_select();
+  // TODO: link dynamically with libcudf_dispatcher.a
+  test_cudf_select();
   return 0;
 }
