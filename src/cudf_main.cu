830f09e395fe3b5384a865dc853817a4e9a5e761
Alexander <aocsa.cs@gmail.com>
2021-06-28 19:19:00 -0500

    add comments

--- src/cudf_main.cu
+++ src/cudf_main.cu
@@ -18,12 +18,16 @@ void test_arrow_select(){
 void test_cudf_select(){
   auto backend = execution_backend(backend_id::CUDF);
 
+  // load all symbols including the static variable defined in cudf/select.cu
   void* handle = dlopen("./libcudf_dispatcher.so", RTLD_LAZY);
   if (!handle) {
       std::cerr << "dlopen error: " << dlerror() << '\n';
       return;
   }
+  // clear errors, find symbol, check errors
   dlerror();
+
+  // Use stub dispatcher
   select_stub(backend, 0, 5, 8);
   // close library
   dlclose(handle);
